8312d919bb76513fe2df9506e6a1e7489620538a
Dvargasar6 <dvargasar6@gmail.com>
2025-05-19 00:33:33 -0500

    Archivos del ejercicio de polimorfismo

---
+++ proyecto_polimorfismo/src/com/Profesores/ProfesorTitular.java
@@ -0,0 +1,7 @@
+package com.Profesores;
+
+public class ProfesorTitular extends Profesor {
+    protected void imprimir() {
+        System.out.println("Es un profesor titular.");
+    }
+}
---
+++ proyecto_polimorfismo/src/com/Profesores/Prueba.java
@@ -0,0 +1,8 @@
+package com.Profesores;
+
+public class Prueba{
+    public static void main(String[] args) {
+        Profesor profesor1 = new ProfesorTitular();
+        profesor1.imprimir();
+    }
+}
